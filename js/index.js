a259e827cec352780d941695da9394770bb44d79
Ocknyer <kdu4045@naver.com>
2023-07-07 00:20:38 +0900

    힌트 추가, 사진 추가

--- js/index.js
+++ js/index.js
@@ -8,8 +8,13 @@ const submitBtn = document.querySelector('.submit-btn');
 const answerUl = document.querySelector('.answer-ul');
 const answerLi = document.querySelector('.answer-li');
 
+const hintBtn = document.querySelector('.hint-btn');
+const hintBox = document.querySelector('.hint-box');
+const hintUl = document.querySelector('.hint-ul');
+
 let currentWord = {};
 const arrAnswer = [];
+const hint = [];
 
 function genWord() {
   const randomNum = ~~(Math.random() * arrWords.length);
@@ -34,14 +39,17 @@ submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   if (currentWord.mean.includes(answer.value)) {
     const list = document.createElement('li');
-    list.innerHTML = `<li class="answe-li">${currentWord.word + '    ' + currentWord.mean[0]}</li>`;
+    list.innerHTML = `<li class="answer-li">${currentWord.word + '    ' + currentWord.mean}</li>`;
     answerUl.append(list);
     answer.value = null;
     genWord();
   } else if (!answer.value) {
     alert('답을 입력하세요');
   } else if (!currentWord.mean.includes(answer.value)) {
-    alert('땡!!!');
+    alert('땡!!! 벌써 힌트를 보시겠습니까???');
+    const list = document.createElement('li');
+    list.innerHTML = `<li class="hint-li">${currentWord.word + '    ' + currentWord.mean}</li>`;
+    hintUl.append(list);
   }
 
   console.log(currentWord);
@@ -49,11 +57,21 @@ submitBtn.addEventListener('click', (e) => {
   console.log(currentWord.mean.includes(answer.value));
 });
 
-wordGenBtn.addEventListener('click', () => {
-  const randomNum = ~~(Math.random() * arrWords.length);
-  currentWord = arrWords[randomNum];
-  question.textContent = currentWord.word;
-  console.log(randomNum);
-});
+// wordGenBtn.addEventListener('click', () => {
+//   const randomNum = ~~(Math.random() * arrWords.length);
+//   currentWord = arrWords[randomNum];
+//   question.textContent = currentWord.word;
+//   console.log(randomNum);
+// });
 
 console.log(currentWord);
+
+hintBtn.addEventListener('click', () => {
+  console.log('hi');
+  hintBox.classList.toggle('hidden');
+});
+
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault();
+  e.returnValue = '';
+});
